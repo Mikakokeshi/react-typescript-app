a5d2bbd9d8190667b36973462fcb00a9342a60ff
Mikako Iijima <mikakokeshi00124@gmail.com>
2022-06-04 15:30:06 +0900

    ログイン画面のレイアウト調整

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>Loginページ　です　</p>;
+  return (
+    <Flex align="center" height="100vh" justify="center">
+      <Box bg="white" w="sm" shadow="md" p={4} borderRadius="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={3} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
