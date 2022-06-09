c54fb7f76407dcacc35cfb9cc44d9a4b97df03cf
Mikako Iijima <mikakokeshi00124@gmail.com>
2022-06-09 23:31:29 +0900

    ユーザー一覧画面の実装

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,39 @@
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { memo, ReactNode, VFC } from "react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+
+  return (
+    <Box
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+      p="4"
+      w="260px"
+      h="260px"
+      borderRadius="10px"
+      shadow="md"
+    >
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full "
+          boxSize="160px"
+          alt={userName}
+          m="auto"
+          src={imageUrl}
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
