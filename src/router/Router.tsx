ed17d830ea3cad5a4d874173f48e84e339073ede
Mikako Iijima <mikakokeshi00124@gmail.com>
2022-06-12 23:59:58 +0900

    added ユーザーログイン情報contextで保持途中

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,33 +5,35 @@ import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
 import { HeaderLayouts } from "../components/templates/HeaderLayouts";
-
+import { LoginUserProvider } from "../providers/LoginUserProvider";
 export const Router: VFC = memo(() => {
   return (
     <Switch>
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route
-        path="/home"
-        render={({ match: { url } }) => (
-          <Switch>
-            {homeRoutes.map((route) => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={`${url}${route.path}`}
-              >
-                <HeaderLayouts>{route.children}</HeaderLayouts>
-              </Route>
-            ))}
-          </Switch>
-        )}
-      />
+      <LoginUserProvider>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route
+          path="/home"
+          render={({ match: { url } }) => (
+            <Switch>
+              {homeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  <HeaderLayouts>{route.children}</HeaderLayouts>
+                </Route>
+              ))}
+            </Switch>
+          )}
+        />
 
-      <Route path="*">
-        <Page404 />
-      </Route>
+        <Route path="*">
+          <Page404 />
+        </Route>
+      </LoginUserProvider>
     </Switch>
   );
 });
